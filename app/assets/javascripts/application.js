c183b247b45dbd92577c4e2ef18d81e727b99463
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-05-13 19:51:22 -0500

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-    $notification = $delete.parentNode;
-    $delete.addEventListener('click', () => {
-      $notification.parentNode.removeChild($notification);
+$(document).on('turbolinks:load', function() {
+  document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      $notification = $delete.parentNode;
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
     });
   });
 });
