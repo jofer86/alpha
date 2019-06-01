f7514d1d9b737a1523b1bdd7f22941db9140c74d
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-06-01 16:27:46 -0500

    final changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only:[:edit, :update, :show]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
+  before_action :requiere_admin, only:[:destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end  
@@ -33,8 +34,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @ser_articles = @user.articles.paginate( page: params[:page], per_page: 5 )
   end
 
+  def destroy
+
+    @user = User.find(params[:id])
+    @ser.destroy
+    flash[:danger] = "user and all articles creted by user have been deleted"
+    redirect_to users_path
+  end
 
 
   private
@@ -48,7 +57,14 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in and !current_user.admin?
+      flash[:danger] = "only admins fool"
       redirect_to root_path
     end
   end
