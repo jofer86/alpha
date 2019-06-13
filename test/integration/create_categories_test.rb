67ab54bd6dfe6e980e1fd7ae5be9ddf57a2c7fbd
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-06-13 14:23:51 -0500

    testing

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+  test "get new category form and create category" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: {name: "sports"} }
+      follow_redirect!
+    end
+  end
+
+  test "Invalid category should fail" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name: " "} }        
+    end 
+  end
+
+  
+  
+end
+
+
